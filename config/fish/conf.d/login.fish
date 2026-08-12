674fc6a89e58495f452d4d5fe27707209dabe099
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-08-12 22:24:10 +0700

    chore: mark tty1 niri autostart as optional

--- config/fish/conf.d/login.fish
+++ config/fish/conf.d/login.fish
@@ -1,5 +1,7 @@
-if status is-login
-    if test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
-        clear && exec niri
-    end
-end
+# optional: auto-start niri on TTY1 login (no display manager / simple)
+# Uncomment if youu use TTY-only
+# if status is-login
+#     if test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
+#         reset && exec niri
+#     end
+# end
