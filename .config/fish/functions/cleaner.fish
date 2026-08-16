d97e150cae7ed9d7f89391ca691bb5be6427a4a2
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-08-16 18:11:30 +0700

    chore: remove duplicate .., ..., .... aliases and delete cleaner

--- .config/fish/functions/cleaner.fish
+++
@@ -1,8 +0,0 @@
-function cleaner
-    echo "Cleaning system..."
-    sudo pacman -Scc --noconfirm
-    sudo journalctl --vacuum-time=2d
-    find ~/.cache /tmp -mindepth 1 -delete 2>/dev/null
-    functions -q paru; and paru -Scc --noconfirm
-    echo "Done."
-end
